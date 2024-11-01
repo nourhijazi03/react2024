89863dc8996920c850425eaac938eb35260d465a
NourH <nour.alhoda.hjz@gmail.com>
2024-11-01 08:29:44 +0200

    update part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Write from "./Components/Write";
 import Read from "./Components/Read";
+import UpdateRead from "./Components/UpdateRead";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Route path="/" element={<Write/>}/>
     <Route path="/write" element={<Write/>}/>
     <Route path="/read" element={<Read/>}/>
+    <Route path="/update" element={<UpdateRead/>}/>
   </Routes>
 </Router>
 
---
+++ src/Components/UpdateRead.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import app from "../firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
+
+function UpdateRead() {
+  let [fruitArray, setFruitArray] = useState([]);
+
+  const fetchData = async () => {
+    const db = getDatabase(app);
+    const dbRef = ref(db, "nature/fruits");
+    const snapshot = await get(dbRef);
+
+    if (snapshot.exists()) {
+      const myData = snapshot.val();
+      const tempArray = Object.keys(myData).map((myFired) => {
+        return {
+          ...myData[myFired],
+          fruitID: myFired,
+        };
+      });
+      setFruitArray(tempArray);
+    } else {
+      alert("error occured");
+    }
+  };
+  return (
+    <div>
+      <h1>Update Data</h1>
+      <button onClick={fetchData}>Show Data</button>
+      <ul>
+        {fruitArray.map((item, index) => (
+          <li key={index}>
+            {item.fruitName}:{item.fruitDef}:{item.fruitID}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UpdateRead;
